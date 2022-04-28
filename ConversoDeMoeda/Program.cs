495fb1c22fa81e26ca1871d28008b831428d5035
Geison Amorim <geisonam43@gmail.com>
2022-04-28 17:23:50 -0300

    Add project files.

---
+++ ConversoDeMoeda/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual e a cotação do dolar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            Console.Write("Quantos dolares voçê ira comprar? ");
+            double quantia = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            double result = ConversoDeMoeda.DolarParaReal(quantia,cotacao);
+
+            Console.WriteLine("Valor a ser pago em reais = R$ "+ result.ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}
